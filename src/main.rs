93fcb6ac17d9d73826b7cc83cae73c6f5b6afde3
Knar <knarxr@protonmail.com>
2024-07-18 12:28:33 -0400

    basic tutorial

--- src/main.rs
+++ src/main.rs
@@ -8,19 +8,19 @@ struct Name(String);
 
 pub struct HelloPlugin;
 
+#[derive(Resource)]
+struct GreetTimer(Timer);
+
 impl Plugin for HelloPlugin {
     fn build(&self, app: &mut App) {
-        app.add_systems(Startup, add_people)
-            .add_systems(Update, (hello_world, (update_people, greet_people).chain()));
+        app.insert_resource(GreetTimer(Timer::from_seconds(2.0, TimerMode::Repeating)))
+            .add_systems(Startup, add_people)
+            .add_systems(Update, (update_people, greet_people).chain());
     }
 }
 
 fn main() {
     App::new().add_plugins((DefaultPlugins, HelloPlugin)).run();
-}
-
-fn hello_world() {
-    println!("hello world!");
 }
 
 fn add_people(mut commands: Commands) {
@@ -29,9 +29,13 @@ fn add_people(mut commands: Commands) {
     commands.spawn((Person, Name("Zayna Nieves".to_string())));
 }
 
-fn greet_people(query: Query<&Name, With<Person>>) {
-    for name in &query {
-        println!("hello {}!", name.0);
+fn greet_people(time: Res<Time>, mut timer: ResMut<GreetTimer>, query: Query<&Name, With<Person>>) {
+    // update our timer with the time elapsed since the last update
+    // if that caused the timer to finish, we say hello to everyone
+    if timer.0.tick(time.delta()).just_finished() {
+        for name in &query {
+            println!("hello {}!", name.0);
+        }
     }
 }
 
