ca0e155e06adda955a66979b7068fcd892bd894e
Knar <knarxr@protonmail.com>
2024-08-19 22:51:13 -0400

    add rapier systems

--- threedee/src/main.rs
+++ threedee/src/main.rs
@@ -2,6 +2,7 @@
 
 use bevy::input::mouse::MouseMotion;
 use bevy::prelude::*;
+use bevy_rapier3d::prelude::*;
 
 #[derive(Component)]
 struct Spinner;
@@ -9,9 +10,35 @@ struct Spinner;
 fn main() {
     App::new()
         .add_plugins(DefaultPlugins)
+        .add_plugins(RapierPhysicsPlugin::<NoUserData>::default())
+        .add_plugins(RapierDebugRenderPlugin::default())
+        .add_systems(Startup, setup_physics)
+        .add_systems(Update, print_ball_altitude)
         .add_systems(Startup, setup)
         .add_systems(Update, (keys, mouse))
         .run();
+}
+
+fn setup_physics(mut commands: Commands) {
+    /* Create the ground. */
+    commands
+        .spawn(Collider::cuboid(100.0, 0.1, 100.0))
+        .insert(TransformBundle::from(Transform::from_xyz(0.0, -2.0, 0.0)));
+
+    /* Create the bouncing ball. */
+    commands
+        .spawn(RigidBody::Dynamic)
+        .insert(Collider::ball(0.5))
+        .insert(Restitution::coefficient(0.7))
+        .insert(TransformBundle::from(Transform::from_xyz(0.0, 4.0, 0.0)));
+}
+
+fn print_ball_altitude(mut positions: Query<&mut Transform, With<RigidBody>>) {
+    for mut transform in positions.iter_mut() {
+        dbg!(transform.rotation.to_axis_angle());
+        transform.rotation = Quat::from_rotation_z(270_f32.to_radians());
+        //println!("Ball altitude: {}", transform.translation.y);
+    }
 }
 
 /// set up a simple 3D scene
