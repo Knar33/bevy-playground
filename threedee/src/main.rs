207bd06b689aceb2f0c0ca7d0ac906461a209f19
Knar <knarxr@protonmail.com>
2024-08-20 21:34:33 -0400

    tweak ball values

--- threedee/src/main.rs
+++ threedee/src/main.rs
@@ -23,13 +23,13 @@ fn setup_physics(mut commands: Commands) {
     /* Create the ground. */
     commands
         .spawn(Collider::cuboid(100.0, 0.1, 100.0))
-        .insert(TransformBundle::from(Transform::from_xyz(0.0, -2.0, 0.0)));
+        .insert(TransformBundle::from(Transform::from_xyz(0.0, 0.0, 0.0)));
 
     /* Create the bouncing ball. */
     commands
         .spawn(RigidBody::Dynamic)
-        .insert(Collider::ball(0.5))
-        .insert(Restitution::coefficient(0.7))
+        .insert(Collider::ball(1.0))
+        .insert(Restitution::coefficient(2.0))
         .insert(TransformBundle::from(Transform::from_xyz(0.0, 4.0, 0.0)));
 }
 
