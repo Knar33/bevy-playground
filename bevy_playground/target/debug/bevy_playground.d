be0afb88acb25c57055c9655291823138c902514
Knar <knarxr@protonmail.com>
2024-09-18 20:42:24 -0400

    renamed project

--- bevy_playground/target/debug/bevy_playground.d
+++
@@ -1,1 +0,0 @@
-/home/knar/Documents/Github/bevy-playground/bevy_playground/target/debug/bevy_playground: /home/knar/Documents/Github/bevy-playground/bevy_playground/src/main.rs
--- bevy_playground/target/debug/deps/bevy_playground-6df41b1f55fc2a68.d
+++
@@ -1,5 +0,0 @@
-/home/knar/Documents/Github/bevy-playground/bevy_playground/target/debug/deps/bevy_playground-6df41b1f55fc2a68: src/main.rs
-
-/home/knar/Documents/Github/bevy-playground/bevy_playground/target/debug/deps/bevy_playground-6df41b1f55fc2a68.d: src/main.rs
-
-src/main.rs:
