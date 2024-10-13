ef023a9edc33c0cb50ade4d441b506edf0424d1d
Knar <knarxr@protonmail.com>
2024-10-12 20:43:29 -0400

    added empty bindings plugin

--- playground/src/main.rs
+++ playground/src/main.rs
@@ -1,12 +1,12 @@
 use bevy::prelude::*;
 
-use crate::plugins::keyboard::KeyboardPlugin;
+use crate::plugins::{bindings::BindingsPlugin, keyboard::KeyboardPlugin};
 
 mod plugins;
 
 fn main() {
     App::new()
         .add_plugins(DefaultPlugins)
-        .add_plugins(KeyboardPlugin)
+        .add_plugins((KeyboardPlugin, BindingsPlugin))
         .run();
 }
---
+++ playground/src/plugins/bindings.rs
@@ -0,0 +1,7 @@
+use bevy::prelude::*;
+
+pub struct BindingsPlugin;
+
+impl Plugin for BindingsPlugin {
+    fn build(&self, app: &mut App) {}
+}
